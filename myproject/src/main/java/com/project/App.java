70cea171d3c119fb496ee1a8b31f0ea38b41a80c
Sphawit_Karik <sphawit.k@ku.th>
2026-02-03 14:18:08 +0700

    Update App.java message

--- myproject/src/main/java/com/project/App.java
+++ myproject/src/main/java/com/project/App.java
@@ -5,6 +5,6 @@ package com.project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("My first Maven Project");
     }
 }
